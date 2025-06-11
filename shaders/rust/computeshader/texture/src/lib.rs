61ba0d4b93dd24aa85e989c060e5a3fb6d9ef4e5
Christian Legnitto <christian@legnitto.com>
2025-06-11 02:17:36 -0700

    Add computeshader rust shaders

---
+++ shaders/rust/computeshader/texture/src/lib.rs
@@ -0,0 +1,33 @@
+#![cfg_attr(target_arch = "spirv", no_std)]
+#![allow(clippy::missing_safety_doc)]
+
+use spirv_std::{spirv, glam::{vec4, Mat4, Vec2, Vec3, Vec4}, Image};
+use spirv_std::image::SampledImage;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Ubo {
+    pub projection: Mat4,
+    pub model: Mat4,
+}
+
+#[spirv(vertex)]
+pub fn main_vs(
+    in_pos: Vec3,
+    in_uv: Vec2,
+    #[spirv(uniform, descriptor_set = 0, binding = 0)] ubo: &Ubo,
+    #[spirv(position)] out_position: &mut Vec4,
+    out_uv: &mut Vec2,
+) {
+    *out_uv = in_uv;
+    *out_position = ubo.projection * ubo.model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
+}
+
+#[spirv(fragment)]
+pub fn main_fs(
+    in_uv: Vec2,
+    #[spirv(descriptor_set = 0, binding = 1)] sampler_color: &SampledImage<Image!(2D, type=f32, sampled)>,
+    out_frag_color: &mut Vec4,
+) {
+    *out_frag_color = sampler_color.sample(in_uv);
+}
