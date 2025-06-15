10691815adf360cd3252f09b3ac4230a75a541b1
Christian Legnitto <christian@legnitto.com>
2025-06-14 17:48:24 -0700

    Add shaderobjects rust shader

---
+++ shaders/rust/shaderobjects/phong/src/lib.rs
@@ -0,0 +1,66 @@
+#![cfg_attr(target_arch = "spirv", no_std)]
+#![allow(clippy::missing_safety_doc)]
+
+use spirv_std::{spirv, glam::{mat3, vec3, vec4, Mat4, Vec3, Vec4}, Image, num_traits::Float};
+use spirv_std::image::SampledImage;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Ubo {
+    pub projection: Mat4,
+    pub model: Mat4,
+    pub light_pos: Vec4,
+}
+
+#[spirv(vertex)]
+pub fn main_vs(
+    in_pos: Vec3,
+    in_normal: Vec3,
+    in_color: Vec3,
+    #[spirv(uniform, descriptor_set = 0, binding = 0)] ubo: &Ubo,
+    #[spirv(position)] out_position: &mut Vec4,
+    out_normal: &mut Vec3,
+    out_color: &mut Vec3,
+    out_view_vec: &mut Vec3,
+    out_light_vec: &mut Vec3,
+) {
+    *out_normal = in_normal;
+    *out_color = in_color;
+    *out_position = ubo.projection * ubo.model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
+    
+    let pos = ubo.model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
+    let model_mat3 = mat3(
+        ubo.model.x_axis.truncate(),
+        ubo.model.y_axis.truncate(),
+        ubo.model.z_axis.truncate(),
+    );
+    *out_normal = model_mat3 * in_normal;
+    let l_pos = model_mat3 * ubo.light_pos.truncate();
+    *out_light_vec = l_pos - pos.truncate();
+    *out_view_vec = -pos.truncate();
+}
+
+#[spirv(fragment)]
+pub fn main_fs(
+    in_normal: Vec3,
+    in_color: Vec3,
+    in_view_vec: Vec3,
+    in_light_vec: Vec3,
+    #[spirv(descriptor_set = 0, binding = 1)] _sampler_color_map: &SampledImage<Image!(2D, type=f32, sampled)>,
+    out_frag_color: &mut Vec4,
+) {
+    // Desaturate color
+    let gray = in_color.dot(vec3(0.2126, 0.7152, 0.0722));
+    let color = in_color.lerp(vec3(gray, gray, gray), 0.65);
+    
+    // High ambient colors because mesh materials are pretty dark
+    let ambient = color * vec3(1.0, 1.0, 1.0);
+    let n = in_normal.normalize();
+    let l = in_light_vec.normalize();
+    let v = in_view_vec.normalize();
+    let r = (-l).reflect(n);
+    let diffuse = n.dot(l).max(0.0) * color;
+    let specular = r.dot(v).max(0.0).powf(32.0) * vec3(0.35, 0.35, 0.35);
+    let final_color = ambient + diffuse * 1.75 + specular;
+    *out_frag_color = vec4(final_color.x, final_color.y, final_color.z, 1.0);
+}
